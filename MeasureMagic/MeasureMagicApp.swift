cf1254bae59c35506732f79a846bc0dc1ca70a97
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-25 14:38:43 -0300

    Initial Commit

---
+++ MeasureMagic/MeasureMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureMagicApp.swift
+//  MeasureMagic
+//
+//  Created by Rodrigo Cavalcanti on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MeasureMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
